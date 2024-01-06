70a7f27bd7035f475a03ad5a88bac4a862168fd6
afsaints0987 <saints0987@gmail.com>
2024-01-06 15:20:30 +0800

    add button

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,15 @@
 
 $(document).ready(function(){
-    // Disabled inspect element
-    // $(document).bind('contextmenu', function(e){
-    //     e.preventDefault();
-    // })
+    //Disabled inspect element
+    $(document).bind('contextmenu', function(e){
+        e.preventDefault();
+    })
 
-    // $(document).keydown(function(e){
-    //     if(e.which === 123){
-    //        return false;
-    //     }
-    // });
+    $(document).keydown(function(e){
+        if(e.which === 123){
+           return false;
+        }
+    });
 
     // Clear input value
 
